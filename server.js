ddacd2707024d0a28440a56bd3fe531263f6608d
Evgeniy <j_savitskii@mail.ru>
2020-06-25 19:08:33 +0300

    Init commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// load env vars
+dotenv.config({
+  path: "./config/config.env",
+});
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`
+  )
+);
